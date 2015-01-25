6d12f200a0e6eb93975dafed1a7fd2036b78e639
Jazz Kersell <jazzkersell@gmail.com>
2015-01-25 17:56:18 -0500

    Generate vector b, fix indexing problem.

--- assgn1/main.c
+++ assgn1/main.c
@@ -12,18 +12,28 @@ void genVector(int n, double *b);
 int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
 
-    double *A = malloc(sizeof *A * m * n);
+    double *A = malloc(sizeof(double) * m * n);
+    double *b = malloc(sizeof(double) * n);
 
     genMatrix(m, n, A);
+    genVector(n, b);
 
-    for (int i = 1; i < m; ++i) {
+    for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
-            printf("%f ", A[i * j]);
+            printf("%f ", A[(i * n) + j]);
         }
         printf("\n");
     }
 
+    printf("\n\n");
+    
+    for (int i = 0; i < n; ++i) {
+        printf("%f ", b[i]);
+    }
+    printf("\n");
+
     free(A);
+    free(b);
     MPI_Finalize();
     return 0; 
 }

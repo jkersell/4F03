cdc89e94525c4421f6f7ed7638092851bb015d9a
Jazz Kersell <jazzkersell@gmail.com>
2015-01-26 10:12:35 -0500

    Only process 0 should generate the values.

--- assgn1/main.c
+++ assgn1/main.c
@@ -9,13 +9,18 @@ void genVector(int n, double *b);
 int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
 
+    int myRank;
     int m = 10;
     int n = 10;
     double *A = malloc(sizeof(double) * m * n);
     double *b = malloc(sizeof(double) * n);
 
-    genMatrix(m, n, A);
-    genVector(n, b);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+
+    if (myRank == 0) {
+        genMatrix(m, n, A);
+        genVector(n, b);
+    }
 
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {

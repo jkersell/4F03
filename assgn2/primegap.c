130239e269166954be2a5e71764e53733468ee28
Jazz Kersell <jazzkersell@gmail.com>
2015-02-06 19:55:20 -0500

    Only initialize on process 0.

--- assgn2/primegap.c
+++ assgn2/primegap.c
@@ -11,11 +11,13 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
     MPI_Comm_size(MPI_COMM_WORLD, &processCount);
 
-    int lowBound = atoi(argv[1]);
-    int highBound = atoi(argv[2]);
+    if (myRank == 0) {
+        int lowBound = atoi(argv[1]);
+        int highBound = atoi(argv[2]);
 
-    if (lowBound < 0) {
-        return 1;
+        if (lowBound < 0) {
+            return 1;
+        }
     }
 
     MPI_Finalize();

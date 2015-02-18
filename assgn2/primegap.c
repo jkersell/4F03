fe75e18dca47dd50ea059838ea7c199e4ad23383
Jazz Kersell <jazzkersell@gmail.com>
2015-02-17 23:57:27 -0500

    Fix type warning.

--- assgn2/primegap.c
+++ assgn2/primegap.c
@@ -23,7 +23,8 @@ void findLargestGap(unsigned int lowBound, unsigned int highBound, Result *resul
         if (mpz_cmp_ui(gap, largestGap) > 0) {
             largestGap = mpz_get_ui(gap);
         }
-        printf("previous: %d, nextPrime: %d, gap: %d, largestGap: %d\n", mpz_get_ui(previous), mpz_get_ui(nextPrime), mpz_get_ui(gap), largestGap);
+        printf("previous: %lu, nextPrime: %lu, gap: %lu, largestGap: %u\n",
+                mpz_get_ui(previous), mpz_get_ui(nextPrime), mpz_get_ui(gap), largestGap);
         mpz_set(previous, nextPrime);
     }
     buildResult(result, largestGap, mpz_get_ui(nextPrime));

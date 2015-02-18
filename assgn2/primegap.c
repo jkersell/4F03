ef10425325929302b6d4a0352062443a22dcb099
Jazz Kersell <jazzkersell@gmail.com>
2015-02-17 20:42:06 -0500

    Determine range for each process to check.

--- assgn2/primegap.c
+++ assgn2/primegap.c
@@ -13,7 +13,8 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
     MPI_Comm_size(MPI_COMM_WORLD, &processCount);
 
-    if (myRank == 0) {
+    int root = 0;
+    if (myRank == root) {
         int lowBound = atoi(argv[1]);
         int highBound = atoi(argv[2]);
 
@@ -23,9 +24,17 @@ int main(int argc, char *argv[]) {
 
         ProcessInfo info;
         buildProcessInfo(&info, myRank, processCount, lowBound, highBound);
-        printf("start: %d, end: %d\n", info.start, info.end);
-    } else {
+        printf("Process 0 start: %d, end: %d\n", info.start, info.end);
 
+        int send[2] = {lowBound, highBound};
+        MPI_Bcast(&send, 2, MPI_INT, root, MPI_COMM_WORLD);
+    } else {
+        int recv[2];
+        MPI_Bcast(&recv, 2, MPI_INT, root, MPI_COMM_WORLD);
+
+        ProcessInfo info;
+        buildProcessInfo(&info, myRank, processCount, recv[0], recv[1]);
+        printf("Process %d start: %d, end: %d\n", myRank, info.start, info.end);
     }
 
     MPI_Finalize();
--- assgn2/processinfo.c
+++ assgn2/processinfo.c
@@ -1,12 +1,17 @@
 #include "processinfo.h"
 
 void buildProcessInfo(ProcessInfo *info, int rank, int processCount, int globalLow, int globalHigh) {
-    int bucketSize = (globalHigh - globalLow) / processCount;
-    int leftover = (globalHigh - globalLow) % processCount;
+    // Range is inclusive so we need to add 1 here to keep both ends
+    int rangeSize = globalHigh - globalLow + 1;
+    int bucketSize = rangeSize / processCount;
+    int leftover = rangeSize % processCount;
 
-    if (rank < leftover) {
-        ++bucketSize;
+    info->start = globalLow;
+    for (int i = 0; i < rank; ++i) {
+        info->start += bucketSize;
+        if (i < leftover) {
+            info->start += 1;
+        }
     }
-    info->start = rank * bucketSize;
-    info->end = info->start + bucketSize - 1;
+    info->end = info->start + (rank < leftover ? bucketSize : bucketSize - 1);
 }
